72ae012085fd4376540653a1ecbc6d4718804962
Sachin Tripathi <56958954+MruDe13@users.noreply.github.com>
2023-11-28 11:03:42 +0545

    changed App into functional components using hooks

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A collection of projects made during the Full Stack Web Development Course using React.
 
-1. First Project is Monsters Rolodex. In this project, I learnt to use React Class component to divide and make independent components in our web app.
+1. First Project is Monsters Rolodex. In this project, I learnt to use React Class component to divide and make independent components in our web app. The second step is to change the Class components into Functional components and make use of react hooks.
--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,55 +1,40 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setfilteredMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newfilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLowerCase().includes(searchField);
     });
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox onSearchChangeHandler={onSearchChange} />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+    setfilteredMonsters(newfilteredMonsters);
+    console.log("filtered monster changed");
+  }, [searchField, monsters]);
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox onSearchChangeHandler={onSearchChange} />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
